fd94782775673a1cd43f6651467df4302b6e0210
Ezra Chung <ezra.chung97@gmail.com>
2023-05-26 12:06:26 -0700

    Initial setup for PersonalSiteApp

---
+++ personalsite2/src/PersonalSiteApp.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./PersonalSiteApp.css";
+
+function PersonalSiteApp(): JSX.Element {
+    // Add your component logic here
+  
+    return (
+      <BrowserRouter>
+
+      </BrowserRouter>
+    );
+  }
+  
+  export default PersonalSiteApp;
