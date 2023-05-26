77df8c66e68d101b8186c0c6769212ce22e680f6
Ezra Chung <ezra.chung97@gmail.com>
2023-05-25 21:43:29 -0700

    edit to initial app test

--- personalsite2/src/App.test.tsx
+++ personalsite2/src/App.test.tsx
@@ -1,9 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component without errors', () => {
   render(<App />);
-  const linkElement = screen.getByText(/count is/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Assert that the logo elements are rendered
+  const viteLogo = screen.getByAltText('Vite logo');
+  const reactLogo = screen.getByAltText('React logo');
+  expect(viteLogo).toBeInTheDocument();
+  expect(reactLogo).toBeInTheDocument();
+
+  // Assert that the text content is rendered
+  const countButton = screen.getByText(/count is/i);
+  const codeElement = screen.getByText(/Edit <code>src\/App.tsx<\/code> and save to test HMR/i);
+  const docsText = screen.getByText(/Click on the Vite and React logos to learn more/i);
+  expect(countButton).toBeInTheDocument();
+  expect(codeElement).toBeInTheDocument();
+  expect(docsText).toBeInTheDocument();
 });
