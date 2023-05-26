2a51493e18480d197a82f698c64ca8caa1c15092
Ezra Chung <ezra.chung97@gmail.com>
2023-05-26 12:06:00 -0700

    Remove React.StrictMode

--- personalsite2/src/main.tsx
+++ personalsite2/src/main.tsx
@@ -4,7 +4,13 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+
 )
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
