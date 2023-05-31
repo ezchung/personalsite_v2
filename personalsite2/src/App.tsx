f34e085b764b89d0dcb2dc5636702804eb456559
Ezra Chung <ezra.chung97@gmail.com>
2023-05-31 13:40:15 -0700

    Render PersonalSiteApp

--- personalsite2/src/App.tsx
+++ personalsite2/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PersonalSiteApp from "./PersonalSiteApp";
 
 
 /** App for rendering personalsite application
@@ -16,9 +17,24 @@ import './App.css'
  */
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
+    <>
+      <div>
+        <PersonalSiteApp />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
+/** Initial rendition 
+ * (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -42,6 +58,4 @@ function App() {
       </p>
     </>
   )
-}
-
-export default App
+ */
