dc5e80dc712f09fbd1ecca853ae9d6c9671f45df
Ezra Chung <ezra.chung97@gmail.com>
2023-05-31 14:00:34 -0700

    Testing if bootstrap is coming through, but seems like its not rn

--- personalsite2/src/Nav.tsx
+++ personalsite2/src/Nav.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
-import userContext from "./userContext";
 
 /** Component for navigation dropdown bar
  *
