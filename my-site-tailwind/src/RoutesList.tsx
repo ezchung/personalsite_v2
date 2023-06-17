adc69b09f3de683db5f5b1b8bfca28b56755de0e
Ezra Chung <ezra.chung97@gmail.com>
2023-06-17 10:29:34 -0700

    RoutesList component for managing routes

---
+++ my-site-tailwind/src/RoutesList.tsx
@@ -0,0 +1,14 @@
+/** Component for managing routes
+ * Props:  
+ *
+ * State:
+ * 
+ * Context: 
+ *
+ * @returns Routes to render components
+ *
+ * PersonalSiteApp
+ *  -> RoutesList
+ *    -> { About,
+ *         Projects }
+ */
